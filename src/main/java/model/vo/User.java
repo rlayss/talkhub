823ed489df5fd8b6c29ac51647e5c433cfe607bb
yeon <skykys8@gmail.com>
2025-03-04 13:12:36 +0900

    feat : user 모델 추가(vo, dao)

---
+++ src/main/java/model/vo/User.java
@@ -0,0 +1,75 @@
+package model.vo;
+
+import java.util.Date;
+
+public class User {
+    private String id;
+    private String password;
+    private String nickname;
+    private String gender;
+    private int birth;
+    private Date createdAt;
+
+    // 기본생성자, 풀생성자(추가 생성자), getter & setter
+
+
+    public User() {
+    }
+
+    public User(String id, String password, String nickname, String gender, int birth, Date createdAt) {
+        this.id = id;
+        this.password = password;
+        this.nickname = nickname;
+        this.gender = gender;
+        this.birth = birth;
+        this.createdAt = createdAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getBirth() {
+        return birth;
+    }
+
+    public void setBirth(int birth) {
+        this.birth = birth;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
